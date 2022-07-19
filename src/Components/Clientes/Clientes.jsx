d1922dc59112d6bd8e0c3090c5b4bbb564679825
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2022-07-19 17:20:19 -0400

    cliente

--- src/Components/Clientes/Clientes.jsx
+++ src/Components/Clientes/Clientes.jsx
@@ -1,17 +1,19 @@
 import React,{useState,useEffect} from 'react';
 import api from '../../Api/api';
 import { ClientesI } from '../../Models/ClientesI';
+import { DataGrid } from '@mui/x-data-grid';
 
 const Clientes = () => {
-    const [cliente, setcliente] = useState([]);
+
+    const [cliente, setCliente] = useState([]);
 
     const getClientes = async () =>{
-
         await api.get('/Cliente').then(res => {
-            const cliente = res.data;
-            console.log(cliente)
-            setcliente({cliente});
+            console.log(res.data)
+            setCliente(res.data)
         }) 
+
+        
     }
 
     useEffect(() => { 
@@ -19,24 +21,36 @@ const Clientes = () => {
     }, []);
 
 
-    // const columns = [
-    //     { field: 'id', headerName: 'ID' },
-    //     { field: 'id', headerName: 'ID' },
-    //     { field: 'id', headerName: 'ID' },
-    //     { field: 'id', headerName: 'ID' },
-    //     { field: 'id', headerName: 'ID' },
-        
-        
-    //   ];
+    const columns = [
+        { field: 'Id', headerName: 'ID' },
+        { field: 'NombreCliente', headerName: 'Nombre Cliente' },
+        { field: 'Cedula', headerName: 'Cedula' },
+        { field: 'Membresia', headerName: 'Membresia' } 
+      ];
 
 
-
+      
 
 
 
     return (
-        <div>
+        <div style={{ height: 700, width: '100%' }}>
             <h1>Clientes</h1>
+            <hr/>
+
+            <div style={{ display: 'flex', height: '100%' }}>
+                <div style={{ flexGrow: 1 }}>
+                    <DataGrid
+                    getRowId={(cliente) => cliente.Id}
+                    rows={cliente}
+                    columns={columns}
+                    pageSize={10}
+                    rowsPerPageOptions={[10]}
+                    />
+                </div>
+            </div>
+
+        
         </div>
     );
 }
